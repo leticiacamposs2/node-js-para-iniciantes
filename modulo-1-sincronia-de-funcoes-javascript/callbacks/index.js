53b6bb521c18b60356a3d0c9103021f3a5e22ff9
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-04-04 15:36:04 -0300

    sincronizando tudo o que for assincrona com o uso de callbacks

--- modulo-1-sincronia-de-funcoes-javascript/callbacks/index.js
+++ modulo-1-sincronia-de-funcoes-javascript/callbacks/index.js
@@ -8,23 +8,28 @@ function obterUsuario(callback) {
     setTimeout(function () {
         return callback(null, {
             id: 1,
-            nome: 'Aladin',
-            dataNascimento: new Date()
-        })
-    }, 1000)
+            nome: 'Leticia',
+            dataNascimento: new Date(),
+        });
+    }, 1000);
 }
 
 function obterTelefone(idUsuario, callback) {
     setTimeout(() => {
         return callback(null, {
-            telefone: '11349494',
-            ddd: 11
-        })
+            telefone: '1199002',
+            ddd: 86,
+        });
     }, 2000);
 }
 
-function obterEndereco(idUsuario) {
-
+function obterEndereco(idUsuario, callback) {
+    setTimeout(function () {
+        return callback(null, {
+            rua: 'Dos bobos',
+            numero: 0,
+        }, 2000);
+    });
 }
 
 function resolverUsuario(erro, usuario) {
@@ -38,8 +43,19 @@ obterUsuario(function resolverUsuario(error, usuario) {
     }
     obterTelefone(usuario.id, function resolverTelefone(error1, telefone) {
         if (error1) {
-            console.error('DEU RUIM EM USUÁRIO', error1)
+            console.error('DEU RUIM EM USUÁRIO', error)
             return;
         }
+        obterEndereco(usuario.id, function resolverEndereco(error2, endereco) {
+            if (error2) {
+                console.log('DEU RUIM EM TELEFONE', error)
+                return;
+            }
+            console.log(`
+                Nome: ${usuario.nome},
+                Endereco: ${endereco.rua}, ${endereco.numero}
+                Telefone: (${telefone.ddd}) ${telefone.telefone}
+            `);
+        })
     })
 })
