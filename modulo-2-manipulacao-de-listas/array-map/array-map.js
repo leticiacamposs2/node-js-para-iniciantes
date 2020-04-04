16fc2a5c3d46260719097ed08097caae0322c720
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-04-04 20:04:12 -0300

    utilizando o map sem a necessidade de variaveis externas

--- modulo-2-manipulacao-de-listas/array-map/array-map.js
+++ modulo-2-manipulacao-de-listas/array-map/array-map.js
@@ -3,10 +3,14 @@ const service = require('./service')
 async function main() {
     try {
         const results = await service.obterPessoas('a')
+        // const names = []
 
-        const names = []
-        results.results.forEach(function (item) {
-            names.push(item.name);
+        // results.results.forEach(function (item) {
+        //     names.push(item.name);
+        // })
+
+        const names = results.results.map(function (pessoa) {
+            return pessoa.name
         })
 
         console.log('names', names)
