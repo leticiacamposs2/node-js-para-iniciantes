f887e560c62cbad4968f172de5a636f063e6bcab
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-04-04 17:45:06 -0300

    utilizando o event emitter

---
+++ modulo-1-sincronia-de-funcoes-javascript/event-emitter/index.js
@@ -0,0 +1,19 @@
+const EventEmitter = require('events')
+
+class MeuEmissor extends EventEmitter {
+
+}
+
+const meuEmissor = new MeuEmissor()
+const nomeEvento = 'usuario:click'
+meuEmissor.on(nomeEvento, function (click) {
+    console.log('um usuario clicou', click)
+})
+
+meuEmissor.emit(nomeEvento, 'na barra de rolagem')
+meuEmissor.emit(nomeEvento, 'no ok')
+
+let count = 0
+setInterval(function () {
+    meuEmissor.emit(nomeEvento, 'no ok' + (count++))
+}, 1000)
