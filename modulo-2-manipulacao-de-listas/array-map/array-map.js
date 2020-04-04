b944c727bb0f674fd999d844929b8547ab41b172
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-04-04 20:12:02 -0300

    criando meu proprio array map

--- modulo-2-manipulacao-de-listas/array-map/array-map.js
+++ modulo-2-manipulacao-de-listas/array-map/array-map.js
@@ -1,5 +1,15 @@
 const service = require('./service')
 
+Array.prototype.meuMap = function(callback) {
+    const novoArrayMapeado = []
+    for(let indice=0; indice <= this.length-1; indice++) {
+        const resultado = callback(this[indice], indice)
+        novoArrayMapeado.push(resultado)
+    }
+
+    return novoArrayMapeado
+}
+
 async function main() {
     try {
         const results = await service.obterPessoas('a')
@@ -9,12 +19,17 @@ async function main() {
         //     names.push(item.name);
         // })
 
+
         // const names = results.results.map(function (pessoa) {
         //     return pessoa.name
         // })
 
 
-        const names = results.results.map((pessoa) => pessoa.names)
+        // const names = results.results.map((pessoa) => pessoa.names)
+
+        const names = results.results.meuMap(function (pessoa, indice) {
+            return `[${ indice }] ${pessoa.name}`
+        })
 
         console.log('names', names)
     } catch (error) {
