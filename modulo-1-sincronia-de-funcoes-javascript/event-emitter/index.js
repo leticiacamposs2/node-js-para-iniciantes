66e9ec6ba81bf195d52f6fd168ef39ab2365cf33
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-04-04 17:53:17 -0300

    recuperando valores digitados no terminal

--- modulo-1-sincronia-de-funcoes-javascript/event-emitter/index.js
+++ modulo-1-sincronia-de-funcoes-javascript/event-emitter/index.js
@@ -10,10 +10,7 @@ meuEmissor.on(nomeEvento, function (click) {
     console.log('um usuario clicou', click)
 })
 
-meuEmissor.emit(nomeEvento, 'na barra de rolagem')
-meuEmissor.emit(nomeEvento, 'no ok')
-
-let count = 0
-setInterval(function () {
-    meuEmissor.emit(nomeEvento, 'no ok' + (count++))
-}, 1000)
+const stdin = process.openStdin()
+stdin.addListener('data', function (value) {
+    console.log(`Você digitou: ${value.toString().trim()}`)
+})
