6623ed3d8f81781712371f9cbb70865e9cb5d461
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-04-04 19:31:38 -0300

    utilizando o for in

--- modulo-2-manipulacao-de-listas/for-forin-forof/for-forin-forof.js
+++ modulo-2-manipulacao-de-listas/for-forin-forof/for-forin-forof.js
@@ -5,15 +5,28 @@ async function main() {
         const result = await service.obterPessoas('a')
         const names = []
 
-        console.time('for')
+        /**
+            console.time('for')
 
-        for(let i=0; i<=result.results.length-1; i++) {
-            const pessoa = result.results[i]
-            names.push(pessoa.name)
-        }
+            for(let i=0; i<=result.results.length-1; i++) {
+                const pessoa = result.results[i]
+                names.push(pessoa.name)
+            }
 
-        console.timeEnd('for')
-        
+            console.timeEnd('for')
+        */
+
+
+            console.time('forin')
+
+            for(let i in result.results) {
+                const pessoa = result.results[i]
+                names.push(pessoa.name)
+            }
+
+            console.timeEnd('forin')
+
+
         console.log('names', names)
     } catch(error) {
         console.error('error interno', error)
