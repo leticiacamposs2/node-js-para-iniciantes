42d7416af8b584b5bee9ed23d5e4b49efd29e8e2
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-04-04 20:35:05 -0300

    utilizando o filter

--- modulo-2-manipulacao-de-listas/array-filter/index.js
+++ modulo-2-manipulacao-de-listas/array-filter/index.js
@@ -1,1 +1,39 @@
-const service = require('../service')
+const { obterPessoas } = require('../service')
+
+/*
+
+{
+    const item = {
+        name: 'Leticia',
+        idade: 25
+    }
+}
+
+const { nome } = item
+console.log(nome)
+
+-- desta forma eu extraio somente o nome da minha constante item
+*/
+
+async function main() {
+    try {
+        const { results } = await obterPessoas('a')
+
+        const familiaLars = results.filter(function (item) {
+            // por padrão precisa retornar um booleano
+            // para informar se deve manter ou remover da lista
+            // false = remove da lista
+            // true = mantém
+            // não encontrou = -1
+            // encontrou = posicaoNoArray
+            const result = item.name.toLowerCase().indexOf('lars') !== -1
+            return result
+        })
+
+        const names = familiaLars.map((pessoa) => pessoa.name)
+        console.log(names)
+
+    } catch (error) {
+        console.error('DEU RUIM', error)
+    }
+}
