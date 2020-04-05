4b80ee9b781956e976b53a6a316bf667f65b0596
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-04-05 20:48:34 -0300

    criando a estrategia do postgres

--- modulo-5-banco-de-dados/design-patterns/src/strategy.js
+++ modulo-5-banco-de-dados/design-patterns/src/strategy.js
@@ -66,3 +66,6 @@ class ContextStrategy {
 
 const contextMongo = new ContextStrategy(new MongoDB())
 contextMongo.create()
+
+const contextPostgres = new ContextStrategy(new Postgres())
+contextPostgres.create()
