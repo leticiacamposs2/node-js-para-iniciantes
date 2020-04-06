3899d3f3a9f9dc210ef74271a2fa30a286c01edd
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-04-05 22:18:29 -0300

    configurando a conexao no script postgres

--- modulo-6-banco-relacional/sequelize/src/db/strategies/postgres.js
+++ modulo-6-banco-relacional/sequelize/src/db/strategies/postgres.js
@@ -3,10 +3,58 @@ const ICrud = require('./interfaces/interfaceCrud')
 class Postgres extends ICrud {
     constructor() {
         super()
+        this._driver = null;
+        this._herois = null;
+        this._connect()
     }
 
-    isConnected() {
-        
+    async isConnected() {
+        try {
+            await this._driver.authenticate()
+            return true;
+        }
+        catch (error) {
+            console.log("Erro:", error)
+            return false;
+        }
+    }
+
+    _connect() {
+        this._driver = new Sequelize(
+            'heroes',
+            'leticiacampos',
+            'minhasenhasecreta',
+            {
+                host: 'localhost',
+                dialect: 'postgres',
+                quoteIdentifiers: false,
+                operatorsAliases: false
+            }
+        )
+    }
+
+    defineModel() {
+        this._herois = driver.define('herois', {
+            id: {
+                type: Sequelize.INTEGER,
+                required: true,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            nome: {
+                type: Sequelize.STRING,
+                required: true
+            },
+            poder: {
+                type: Sequelize.STRING,
+                required: true
+            }
+        }, {
+            tableName: 'TB_HEROIS',
+            freezeTableName: false,
+            timestamps: false
+        })
+        await Herois.sync()
     }
 
     create(item) {
